43ae828dd4c63ec9edbe2111dd260b3fc27146c4
hyunooss <dlgusdn9098@gmail.com>
2022-10-13 19:15:24 +0900

    Create test.py
    test.py 생성

---
+++ src/test.py
@@ -0,0 +1,13 @@
+import sys
+
+while True:
+    line = list(map(int, sys.stdin.readline().split()))
+    if(line[0] == 0):
+        break
+
+    hypo = max(line)
+    line.remove(hypo)
+    if line[0]**2 + line[1]**2 == hypo**2:
+        print("right")
+    else:
+        print('wrong')
